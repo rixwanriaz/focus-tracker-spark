c81edbff0cde3b6f9721fcab69e4776ae9250889
rizwan <rixwan.riaz123@gmail.com>
2025-10-20 02:26:01 +0500

    fix

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -147,15 +147,13 @@ const Projects: React.FC = () => {
         {/* Header */}
         <div className="flex items-center justify-between px-6 py-6 border-b border-gray-800">
           <h1 className="text-2xl font-bold">Projects</h1>
-          <PermissionGate permission="projects:write">
-            <Button
-              onClick={() => setDialogOpen(true)}
-              className="bg-purple-600 hover:bg-purple-700 text-white"
-            >
-              <Plus className="mr-2 h-4 w-4" />
-              New project
-            </Button>
-          </PermissionGate>
+          <Button
+            onClick={() => setDialogOpen(true)}
+            className="bg-purple-600 hover:bg-purple-700 text-white"
+          >
+            <Plus className="mr-2 h-4 w-4" />
+            New project
+          </Button>
         </div>
 
         {/* Filters */}
